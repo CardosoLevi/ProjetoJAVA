6f93345705f40f5d6c4ae3002d2fdc675e2898fc
Levi <levifcard@gmail.com>
2023-02-16 19:44:24 -0300

    deletou a classe main e adcionou alguns pacotes criando uma classe e tipos de dados.

---
+++ src/basico/TiposDeDados/TiposDeDados.java
@@ -0,0 +1,8 @@
+package basico.TiposDeDados;
+
+public class TiposDeDados {
+    public static void main(String[] args) {
+        int variavelInteiro = 1000000;
+        double variavelDouble = 1000.9999999999;
+    }
+}
